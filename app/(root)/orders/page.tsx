1ad41bd58c6c9bdff9208ff77c0eead9505e93ec
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-07-09 13:46:15 +0100

    Implement order page

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,6 +1,88 @@
+import Image from "next/image";
+
 const Orders = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">Orders</div>
-  )
-}
-export default Orders
+    <div className="flex flex-col min-h-screen">
+      <div className="wrapper mt-20">
+        <table className="w-full text-sm text-left ">
+          <tbody>
+            <tr className="border-b">
+              <td className="px-6 py-4 items-center flex max-sm:flex-col">
+                <Image
+                  src="/images/item6.png"
+                  alt="Product Image"
+                  width={210}
+                  height={180}
+                  className="rounded-md object-cover"
+                />
+                <div>
+                  <p className="font-manrope sm:text-[20px] text-xs font-bold sm:leading-[27.32px] text-left text-[#8D7A6B]">
+                    Coconut Oil
+                  </p>
+                  <p className="font-manrope sm:text-[12px] text-xs sm:leading-[27.32px] mt-5 text-left text-[#A3D9A5]">
+                    1pcs
+                  </p>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">$20</td>
+
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">
+                Processing
+              </td>
+            </tr>
+
+            <tr className="border-b">
+              <td className="px-6 py-4 items-center flex max-sm:flex-col">
+                <Image
+                  src="/images/item2.png"
+                  alt="Product Image"
+                  width={210}
+                  height={180}
+                  className="rounded-md object-cover"
+                />
+                <div>
+                  <p className="font-manrope sm:text-[20px] text-xs font-bold sm:leading-[27.32px] text-left text-[#8D7A6B]">
+                    Virgin Oil
+                  </p>
+                  <p className="font-manrope sm:text-[12px] text-xs sm:leading-[27.32px] mt-5 text-left text-[#A3D9A5]">
+                    2pcs
+                  </p>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">$50</td>
+
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">
+                Processing
+              </td>
+            </tr>
+            <tr className="border-b">
+              <td className="px-6 py-4 items-center flex max-sm:flex-col">
+                <Image
+                  src="/images/item13.png"
+                  alt="Product Image"
+                  width={210}
+                  height={180}
+                  className="rounded-md object-cover"
+                />
+                <div>
+                  <p className="font-manrope sm:text-[20px] text-xs font-bold sm:leading-[27.32px] text-left text-[#8D7A6B]">
+                    Snake Oil
+                  </p>
+                  <p className="font-manrope sm:text-[12px] text-xs sm:leading-[27.32px] mt-5 text-left text-[#A3D9A5]">
+                    4pcs
+                  </p>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">$100</td>
+
+              <td className="px-6 py-4 font-medium text-[#8D7A6B]">
+                Processing
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default Orders;
